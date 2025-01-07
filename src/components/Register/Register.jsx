5998f002080353b2293aab4eabe309084c27e712
fselmi <francois-foued.selmi@universite-paris-saclay.fr>
2025-01-07 19:47:05 +0100

    connexion et inscription front

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,77 +1,154 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Register.css';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 export default function Register() {
+  // Utilisation de useState pour gérer les valeurs des champs de formulaire
+  const [email, setEmail] = useState('');
+  const [pseudo, setPseudo] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [birthdate, setBirthdate] = useState('');
+  const [gender, setGender] = useState('');  // Champ pour le genre
+
+  const navigate = useNavigate();
+
+  // Fonction pour gérer le changement des champs du formulaire
+ const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  // Validation de base des champs
+  if (password !== confirmPassword) {
+    alert('Les mots de passe ne correspondent pas');
+    return;
+  }
+
+  const userData = {
+    email,
+    pseudo,
+    password,
+    birthdate,
+    gender,  // Inclure le genre dans les données envoyées
+  };
+
+  try {
+    // Récupérer le token CSRF à partir des cookies
+    const csrftoken = document.cookie.match(/csrftoken=([\w-]+)/)?.[1];
+    console.log(csrftoken);  // Check if the CSRF token is retrieved
+
+    const response = await fetch('http://127.0.0.1:8000/register/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRFToken': csrftoken,  // Ajouter le token CSRF
+      },
+      body: JSON.stringify(userData),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      alert('Inscription réussie');
+      navigate('/signin');
+    } else {
+      alert(data.message || 'Erreur d\'inscription');
+    }
+  } catch (error) {
+    console.error('Erreur:', error);
+    alert('Une erreur est survenue. Veuillez réessayer.');
+  }
+};
+
+
   return (
     <div className="mainContainer">
       <div className="titleContainer">
         <div>S'INSCRIRE</div>
       </div>
       <br />
-      
-      <div className="inputContainer">
-        <input
-          placeholder="Email"
-          className="inputBox"
-        />
-      </div>
-      <br />
 
-      <div className="inputContainer">
-        <input
-          placeholder="Pseudo"
-          className="inputBox"
-        />
-      </div>
-      <br />
-
-      <div className="inputContainer">
-        <input
-          type="password"
-          placeholder="Mot de passe"
-          className="inputBox"
-        />
-      </div>
-      <br />
-
-      <div className="inputContainer">
-        <input
-          type="password"
-          placeholder="Confirmer le mot de passe"
-          className="inputBox"
-        />
-      </div>
-      <br />
-
-      <div className="inputContainer">
-        <p>Date de naissance</p>
-        <input
-          type="date"
-          className="inputBox dateInput"
-        />
-      </div>
-      <br />
-      <div className="genderContainer">
-        <button className="genderButton">Femme</button>
-        <button className="genderButton">Homme</button>
-      </div>
-
-      <br />
-      <div className="buttonContainer">
-        <input
-          className="inputButton"
-          type="button"
-          value="Je m'inscris"
-        />
-        <NavLink to={"/signin"}>
+      <form onSubmit={handleSubmit}>
+        <div className="inputContainer">
           <input
-            className="inputButton-Sgn"
-            type="button"
-            value="Je me connecte"
+            type="email"
+            placeholder="Email"
+            className="inputBox"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
           />
-        </NavLink>
-      </div>
+        </div>
+        <br />
+
+        <div className="inputContainer">
+          <input
+            type="text"
+            placeholder="Pseudo"
+            className="inputBox"
+            value={pseudo}
+            onChange={(e) => setPseudo(e.target.value)}
+            required
+          />
+        </div>
+        <br />
+
+        <div className="inputContainer">
+          <input
+            type="password"
+            placeholder="Mot de passe"
+            className="inputBox"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <br />
+
+        <div className="inputContainer">
+          <input
+            type="password"
+            placeholder="Confirmer le mot de passe"
+            className="inputBox"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+        </div>
+        <br />
+
+        <div className="inputContainer">
+          <p>Date de naissance</p>
+          <input
+            type="date"
+            className="inputBox dateInput"
+            value={birthdate}
+            onChange={(e) => setBirthdate(e.target.value)}
+            required
+          />
+        </div>
+        <br />
+
+        <div className="genderContainer">
+          <button type="button" className="genderButton" onClick={() => setGender('M')}>Homme</button>
+          <button type="button" className="genderButton" onClick={() => setGender('F')}>Femme</button>
+        </div>
+
+        <br />
+        <div className="buttonContainer">
+          <input
+            className="inputButton"
+            type="submit"
+            value="Je m'inscris"
+          />
+          <NavLink to={"/signin"}>
+            <input
+              className="inputButton-Sgn"
+              type="button"
+              value="Je me connecte"
+            />
+          </NavLink>
+        </div>
+      </form>
     </div>
   );
 }
--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './SignIn.css'
-import { NavLink } from "react-router-dom"
-
+import { NavLink, useNavigate } from "react-router-dom";
 
 export default function SignIn() {
+  const [emailOrPseudo, setEmailOrPseudo] = useState('');
+  const [password, setPassword] = useState('');
+  const [pseudo, setPseudo] = useState('');
+  const navigate = useNavigate();
+
+
+  // Function to get CSRF token from cookies
+  const getCSRFToken = () => {
+    const cookies = document.cookie.split(';');
+    for (let cookie of cookies) {
+      cookie = cookie.trim();
+      if (cookie.startsWith('csrftoken=')) {
+        return cookie.substring('csrftoken='.length);
+      }
+    }
+    return null;
+  };
+
+  const handleLogin = async () => {
+    const csrfToken = getCSRFToken();  // Récupérer le token CSRF
+  
+    const response = await fetch('http://localhost:8000/login/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json', // Type de contenu en JSON
+        'X-CSRFToken': csrfToken,  // Ajoutez le token CSRF dans l'en-tête
+      },
+      body: JSON.stringify({
+        emailOrPseudo: emailOrPseudo,  // Récupère le pseudo ou l'email depuis le formulaire
+        password: password,
+        pseudo:pseudo          // Récupère le mot de passe depuis le formulaire
+      }),
+      credentials: 'include',  // Assurez-vous que les cookies sont envoyés avec la requête
+    });
+  
+    const data = await response.json();
+  
+    if (response.ok) {
+      navigate('/');
+      // Rediriger ou effectuer d'autres actions
+
+    } else {
+      alert(data.error || "An error occurred");
+    }
+  };
+
   return (
     <div className='mainContainer'>
       <div className='titleContainer'>
@@ -12,42 +57,41 @@ export default function SignIn() {
       <br />
       <div className='inputContainer'>
         <input
-        //   value={email}
+          value={emailOrPseudo}
+          onChange={(e) => setEmailOrPseudo(e.target.value)}
           placeholder="Email ou pseudo"
-        //   onChange={()}
           className='inputBox'
         />
-        {/* <label className="errorLabel">{emailError}</label> */}
       </div>
       <br />
       <div className='inputContainer'>
         <input
-        //   value={password}
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           placeholder="Mot de passe"
-        //   onChange={()}
           className='inputBox'
         />
-        {/* <label className="errorLabel">{passwordError}</label> */}
       </div>
       <br />
       <a>Mot de passe oublié ?</a>
       <div>
         <div className="buttonContainer">
-            <input
+          <input
             className="inputButton"
             type="button"
             value="Se connecter"
-            />
-            <NavLink to={"/register"}>
-              <input
+            onClick={handleLogin}
+          />
+          <NavLink to="/register">
+            <input
               className="inputButton"
               type="button"
               value="Créer un compte"
-              />
-            </NavLink>
+            />
+          </NavLink>
         </div>
       </div>
-
     </div>
-  )
+  );
 }
